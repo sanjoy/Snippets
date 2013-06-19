7fd211b1aaf726b2db8a5f1e708e813d64c157b4
Sanjoy Das <sanjoy@playingwithpointers.com>
2013-06-19 15:00:49 +0530

    BiasedLocking.cc: halfway through porting to C++11.

--- BiasedLocking.cc
+++ BiasedLocking.cc
@@ -4,6 +4,10 @@
 #include <cassert>
 #include <cstdlib>
 
+#include <atomic>
+#include <mutex>
+#include <thread>
+
 #include "nanotimer.hpp"
 
 using namespace std;
@@ -37,28 +41,6 @@ const long kBackoffUsecsDelayLimit = 1024 * 8;
       if (backoff < (kBackoffUsecsDelayLimit / 2)) backoff *= 2;        \
     }                                                                   \
   } while(false)
-
-class PthreadLock {
- public:
-  PthreadLock() {
-    pthread_mutex_init(&mutex_, NULL);
-  }
-
-  void lock() {
-    pthread_mutex_lock(&mutex_);
-  }
-
-  void unlock() {
-    pthread_mutex_unlock(&mutex_);
-  }
-
-  ~PthreadLock() {
-    pthread_mutex_destroy(&mutex_);
-  }
-
- private:
-  pthread_mutex_t mutex_;
-};
 
 class BiasedLock {
  public:
@@ -138,92 +120,55 @@ class BiasedLock {
     kStateDefault
   };
 
-  PthreadLock base_lock_;
+  mutex base_lock_;
   pthread_t biasing_thread_id_;
 
   volatile unsigned state_;
   volatile bool revoke_requested_;
 };
 
-class Thread {
- public:
-  void start() {
-    pthread_create(&thread_, NULL, launch_thread, this);
+template<typename LockTy>
+static void test_thread_body(LockTy *the_lock, volatile bool *shared_flag,
+                             const int iterations_count,
+                             const int ms_wait_time) {
+  for (int i = 0; i < iterations_count; i++) {
+    if (ms_wait_time != 0) usleep(1000 * ms_wait_time);
+
+    the_lock->lock();
+
+    ArbitraryDelay();
+    if (UNLIKELY(*shared_flag)) goto no_mutex;
+
+    *shared_flag = true;
+    ArbitraryDelay();
+    if (UNLIKELY(!*shared_flag)) goto no_mutex;
+
+    *shared_flag = false;
+    ArbitraryDelay();
+    if (UNLIKELY(*shared_flag)) goto no_mutex;
+
+    the_lock->unlock();
   }
+  return;
 
-  void* join() {
-    void *return_value;
-    pthread_join(thread_, &return_value);
-    return return_value;
-  }
-
- protected:
-  virtual void* run() = 0;
-
- private:
-  pthread_t thread_;
-
-  static void* launch_thread(void *thread_obj) {
-    return reinterpret_cast<Thread *>(thread_obj)->run();
-  }
-};
+no_mutex:
+  printf("error: no mutual exclusion!");
+  abort();
+}
 
 template<typename LockTy>
-class TestThread : public Thread {
- public:
-  TestThread(LockTy *the_lock, volatile bool *shared_flag, const int run_count,
-             const int ms_wait_time)
-      : the_lock_(the_lock),
-        shared_flag_(shared_flag),
-        run_count_(run_count),
-        ms_wait_time_(ms_wait_time) { }
-
- protected:
-  virtual void* run() {
-    for (int i = 0; i < run_count_; i++) {
-      if (ms_wait_time_ != 0) usleep(1000 * ms_wait_time_);
-
-      the_lock_->lock();
-
-      ArbitraryDelay();
-      if (UNLIKELY(*shared_flag_)) goto no_mutex;
-
-      *shared_flag_ = true;
-      ArbitraryDelay();
-      if (UNLIKELY(!*shared_flag_)) goto no_mutex;
-
-      *shared_flag_ = false;
-      ArbitraryDelay();
-      if (UNLIKELY(*shared_flag_)) goto no_mutex;
-
-      the_lock_->unlock();
-    }
-    return NULL;
- no_mutex:
-    printf("error: no mutual exclusion!");
-    abort();
-  }
-
- private:
-  LockTy *the_lock_;
-  volatile bool *shared_flag_;
-  const int run_count_;
-  const int ms_wait_time_;
-};
-
-template<typename LockTy>
-static void simple_bench(string kind, int iteration_count) {
+static void simple_bench(string kind, int iterations_count,
+                         int second_thread_wait_time_ms) {
   NanoTimer timer(kind);
   volatile bool shared_flag = false;
   LockTy the_lock;
 
-  TestThread<LockTy> fast_thread(&the_lock, &shared_flag, iteration_count, 0);
-  TestThread<LockTy> slow_thread_a(&the_lock, &shared_flag, 1, 14);
-  TestThread<LockTy> slow_thread_b(&the_lock, &shared_flag, 1, 13);
-
-  fast_thread.start();
-  slow_thread_a.start();
-  slow_thread_b.start();
+  thread fast_thread(test_thread_body<LockTy>, &the_lock, &shared_flag,
+                     iterations_count, 0);
+  thread slow_thread_a(test_thread_body<LockTy>, &the_lock, &shared_flag, 1,
+                       second_thread_wait_time_ms);
+  thread slow_thread_b(test_thread_body<LockTy>, &the_lock, &shared_flag, 1,
+                       second_thread_wait_time_ms);
 
   fast_thread.join();
   slow_thread_a.join();
@@ -239,8 +184,10 @@ class NoLock {
 int main() {
   const int kIterationCount = kCheckThoroughly ? 1024 : (1024 * 1024);
   while (true) {
-    simple_bench<PthreadLock>("simple_bench; pthread_lock", kIterationCount);
-    simple_bench<BiasedLock>("simple_bench; biased_lock", kIterationCount);
+    simple_bench<mutex>("simple_bench; pthread_lock", kIterationCount,
+                        14);
+    simple_bench<BiasedLock>("simple_bench; biased_lock", kIterationCount,
+                             14);
     /// The following should call abort(), if kCheckThoroughly is true
     // simple_bench<NoLock>("simple_bench; no_lock", kIterationCount);
   }
